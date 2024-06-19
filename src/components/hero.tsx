d5ade0b47bd64c9baca98ef6277722f03eb71825
ousama <ousamaistaken@gmail.com>
2024-06-19 23:38:15 +0100

    testin

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,8 +8,8 @@ export const Hero = () => {
   return (
     <div className="min-h-screen">
     <div className="absolute bottom-0 left-5">
-      <h1 className={`text-[260px] tracking-[-0.020em] leading-[190px] ${Robuck.className}`}>
-        FRONTEND DEVELOPER
+      <h1 className={`text-[260px] tracking-[-0.015em] leading-[190px] uppercase ${Robuck.className}`}>
+        software developer
       </h1>
     </div>
     </div>
